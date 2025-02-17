8fc2aea0548491797f7d29dc6dcaa0796505819f
Egor Vereshko <egor.vereshko@mail.ru>
2025-02-17 13:00:26 +0500

    fix syntax

--- src/cli/command-parser.ts
+++ src/cli/command-parser.ts
@@ -1,18 +1,18 @@
 type ParsedCommand = Record<string, string[]>
 
 export class CommandParser {
-    static parse(cliArguments: string[]): ParsedCommand {
-        const parsedCommand: ParsedCommand = {};
-        let currentComand = '';
+  static parse(cliArguments: string[]): ParsedCommand {
+    const parsedCommand: ParsedCommand = {};
+    let currentComand = '';
 
-        for (const argument of cliArguments) {
-            if (argument.startsWith('--')) {
-                parsedCommand[argument] = [];
-                currentComand = argument;
-            } else if (currentComand && argument) {
-                parsedCommand[currentComand].push(argument);
-            }
-        }
+    for (const argument of cliArguments) {
+      if (argument.startsWith('--')) {
+        parsedCommand[argument] = [];
+        currentComand = argument;
+      } else if (currentComand && argument) {
+        parsedCommand[currentComand].push(argument);
+      }
+    }
 
     return parsedCommand;
   }
