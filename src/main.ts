cbce80ee25644e90596f8aec44c707c9163357db
Egor Vereshko <egor.vereshko@mail.ru>
2025-05-06 14:48:58 +0500

    feat: realise --generate and update --import commands

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { CLIApplication } from './cli/cli-app.js';
 import { HelpCommand } from './cli/commands/help.command.js';
 import { VersionCommand } from './cli/commands/version.command.js';
 import { ImportCommand } from './cli/commands/import.command.js';
+import { GenerateCommand } from './cli/commands/generate.command.js';
 
 function bootstrap() {
     const cliApplication = new CLIApplication();
@@ -11,6 +12,7 @@ function bootstrap() {
         new HelpCommand(),
         new VersionCommand(),
         new ImportCommand(),
+        new GenerateCommand(),
     ]);
     cliApplication.processCommand(process.argv);
 }
