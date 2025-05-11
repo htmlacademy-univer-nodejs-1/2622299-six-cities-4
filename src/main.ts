279da349d4439bf0ce59598899f413681ce34867
Egor Vereshko <egor.vereshko@mail.ru>
2025-05-11 11:53:06 +0500

    feat: add database integration and implement --import command for data import

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HelpCommand } from './cli/commands/help.command.js';
 import { VersionCommand } from './cli/commands/version.command.js';
 import { ImportCommand } from './cli/commands/import.command.js';
 import { GenerateCommand } from './cli/commands/generate.command.js';
+import 'reflect-metadata';
 
 function bootstrap() {
   const cliApplication = new CLIApplication();
